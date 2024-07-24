601b55fb37e084ec4acd77fa307cd9535d418ef5
Dev-Willer <josewiller2018@gmail.com>
2024-07-23 21:52:13 -0300

    Commit Inicial

---
+++ soma.js
@@ -0,0 +1,12 @@
+// Importando uma Biblioteca...
+//const bibliotecaQualquer = require('express') // --> Utilizando o commonsJS
+//import bibliotecaQualquer from 'express' // --> Utilizando o es modules
+
+
+
+
+function soma (numero1, numero2) {
+    return numero1 + numero2;
+}
+
+module.exports = soma
