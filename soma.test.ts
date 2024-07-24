a542172926e4f22f1a8018c64677baa0a541ca71
Dev-Willer <josewiller2018@gmail.com>
2024-07-23 22:50:02 -0300

    Implementação da função subtração com testes

--- soma.test.ts
+++ soma.test.ts
@@ -3,11 +3,11 @@ import soma from './soma';
 describe('Testes de soma', () => {
 
     test('Deve verificar o resultado de uma soma', ()=> {
-        expect(soma(3,8)).toBe(11)
+        expect(soma(3,8)).toBeCloseTo(11)
     })
 
     test('Deve verificar o resultado de uma soma', ()=> {
-        expect(soma(3,8)).not.toBe(12)
+        expect(soma(3,8)).not.toBeCloseTo(12)
     })
 })
 
